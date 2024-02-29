28cdb697188967e8f27bd53c656f5a02c93b5b9d
Bryan <125blopez@frhsd.com>
2024-02-29 08:37:18 -0500

    Some js

---
+++ js/script.js
@@ -0,0 +1,7 @@
+function one(){
+    document.getElementById('output').innerHTML = "On January 2, 366, the Alemanni yet again crossed the frozen Rhine in large numbers, to invade the Gallic provinces, this time being defeated by Valentinian (see Battle of Solicinium).";
+}
+
+function two(){
+    document.getElementById('title').innerHTML = "January 2, 366";
+}
