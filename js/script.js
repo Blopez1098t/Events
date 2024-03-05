231b7c5f0b1aa897cbc75e7b6cb44312d0595898
Bryan <125blopez@frhsd.com>
2024-03-05 08:35:34 -0500

    Somemore js

--- js/script.js
+++ js/script.js
@@ -9,3 +9,27 @@ function two(){
 function three(){
     document.getElementById('img').src="img/366.jpg"
 }
+
+function one1(){
+    document.getElementById('output').innerHTML = "Luna 1 (launched January 2, 1959) was the first spacecraft to escape Earth's gravity. It failed to impact the Moon as planned and became the first artificial object to go into orbit around the Sun.";
+}
+
+function two1(){
+    document.getElementById('title').innerHTML = "January 2, 1959";
+}
+
+function three1(){
+    document.getElementById('img').src="img/1959.jpg"
+}
+
+function one2(){
+    document.getElementById('output').innerHTML = "On January 2, 366, the Alemanni yet again crossed the frozen Rhine in large numbers, to invade the Gallic provinces, this time being defeated by Valentinian (see Battle of Solicinium).";
+}
+
+function two2(){
+    document.getElementById('title').innerHTML = "January 2, 366";
+}
+
+function three2(){
+    document.getElementById('img').src="img/366.jpg"
+}
