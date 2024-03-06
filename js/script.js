fc9a69eb231bf3ebfc0d5a04ade4b6cf8e235368
Bryan Lopez <125blopez@frhsd.com>
2024-03-05 19:50:16 -0500

    finished js I think

--- js/script.js
+++ js/script.js
@@ -11,25 +11,110 @@ function three(){
 }
 
 function one1(){
-    document.getElementById('output').innerHTML = "Luna 1 (launched January 2, 1959) was the first spacecraft to escape Earth's gravity. It failed to impact the Moon as planned and became the first artificial object to go into orbit around the Sun.";
+    document.getElementById('output').innerHTML = "On 2 January 1860, French astronomer Urbain Le Verrier announced the discovery of Vulcan, a hypothetical planet inside the Mercury orbit, to a meeting of the Académie des Sciences in Paris. Despite the lack of any reliable observation, Le Verrier really was convinced until his death that he had discovered a new planet.";
 }
 
 function two1(){
-    document.getElementById('title').innerHTML = "January 2, 1959";
+    document.getElementById('title').innerHTML = "January 2, 1860";
 }
 
 function three1(){
-    document.getElementById('img').src="img/1959.jpg"
+    document.getElementById('img').src="img/1860.jpg"
 }
 
 function one2(){
-    document.getElementById('output').innerHTML = "On January 2, 366, the Alemanni yet again crossed the frozen Rhine in large numbers, to invade the Gallic provinces, this time being defeated by Valentinian (see Battle of Solicinium).";
+    document.getElementById('output').innerHTML = "Luna 1 (launched January 2, 1959) was the first spacecraft to escape Earth's gravity. It failed to impact the Moon as planned and became the first artificial object to go into orbit around the Sun.";
 }
 
 function two2(){
-    document.getElementById('title').innerHTML = "January 2, 366";
+    document.getElementById('title').innerHTML = "January 2, 1959";
 }
 
 function three2(){
-    document.getElementById('img').src="img/366.jpg"
+    document.getElementById('img').src="img/1959.jpg"
+}
+
+function one3(){
+    document.getElementById('output').innerHTML = "Reagan was elected governor in 1966 with 57.5 percent of the vote. Reagan was sworn in on January 2, 1967 at ten minutes past midnight—starting early to prevent Brown from naming judges at the last minute.";
+}
+
+function two3(){
+    document.getElementById('title').innerHTML = "January 2, 1967";
+}
+
+function three3(){
+    document.getElementById('img').src="img/1967.jpg"
+}
+
+function one4(){
+    document.getElementById('output').innerHTML = "It was on January 2, 1981 that the man who had brutally murdered 13 women was finally snared - although it would be several days before the driver who had been arrested in Melbourne Avenue, Broomhill would be revealed as the deadly serial killer who had struck fear into northern England for years.";
+}
+
+function two4(){
+    document.getElementById('title').innerHTML = "January 2, 1981";
+}
+
+function three4(){
+    document.getElementById('img').src="img/1981.jpg"
+}
+
+function one5(){
+    document.getElementById('output').innerHTML = "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proide";
+}
+
+function two5(){
+    document.getElementById('title').innerHTML = "January 2, 2006";
+}
+
+function three5(){
+    document.getElementById('img').src="img/2006.webp"
+}
+
+function cbgc(){
+    document.querySelector(".but1").style.backgroundColor= "#BA181B"; 
+    document.querySelector(".but2").style.backgroundColor= "#A4161A"; 
+    document.querySelector(".but3").style.backgroundColor= "#660708"; 
+    document.querySelector(".but4").style.backgroundColor= "#A4161A"; 
+    document.querySelector(".but5").style.backgroundColor= "#660708"; 
+    document.querySelector(".but6").style.backgroundColor= "#3D0505"; 
+}
+function cbgc2(){
+    document.querySelector(".but1").style.backgroundColor= "#A4161A"; 
+    document.querySelector(".but2").style.backgroundColor= "#BA181B"; 
+    document.querySelector(".but3").style.backgroundColor= "#A4161A"; 
+    document.querySelector(".but4").style.backgroundColor= "#660708"; 
+    document.querySelector(".but5").style.backgroundColor= "#A4161A"; 
+    document.querySelector(".but6").style.backgroundColor= "#660708"; 
+}
+function cbgc3(){
+    document.querySelector(".but1").style.backgroundColor= "#660708"; 
+    document.querySelector(".but2").style.backgroundColor= "#A4161A"; 
+    document.querySelector(".but3").style.backgroundColor= "#BA181B"; 
+    document.querySelector(".but4").style.backgroundColor= "#3D0505"; 
+    document.querySelector(".but5").style.backgroundColor= "#660708"; 
+    document.querySelector(".but6").style.backgroundColor= "#A4161A"; 
+}
+function cbgc4(){
+    document.querySelector(".but1").style.backgroundColor= "#A4161A"; 
+    document.querySelector(".but2").style.backgroundColor= "#660708"; 
+    document.querySelector(".but3").style.backgroundColor= "#3D0505"; 
+    document.querySelector(".but4").style.backgroundColor= "#BA181B"; 
+    document.querySelector(".but5").style.backgroundColor= "#A4161A"; 
+    document.querySelector(".but6").style.backgroundColor= "#660708"; 
+}
+function cbgc5(){
+    document.querySelector(".but1").style.backgroundColor= "#660708"; 
+    document.querySelector(".but2").style.backgroundColor= "#A4161A"; 
+    document.querySelector(".but3").style.backgroundColor= "#660708"; 
+    document.querySelector(".but4").style.backgroundColor= "#A4161A"; 
+    document.querySelector(".but5").style.backgroundColor= "#BA181B"; 
+    document.querySelector(".but6").style.backgroundColor= "#A4161A"; 
+}
+function cbgc6(){
+    document.querySelector(".but1").style.backgroundColor= "#3D0505"; 
+    document.querySelector(".but2").style.backgroundColor= "#660708"; 
+    document.querySelector(".but3").style.backgroundColor= "#A4161A"; 
+    document.querySelector(".but4").style.backgroundColor= "#660708"; 
+    document.querySelector(".but5").style.backgroundColor= "#A4161A"; 
+    document.querySelector(".but6").style.backgroundColor= "#BA181B"; 
 }
